d122266b5bb1fd545e9dda0da4b27de6d37b41a1
Alex Bazhanau <bazhanau@outlook.com>
2018-12-17 15:14:40 -0800

    more E2E tests added

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { ethers, Contract } from 'ethers';
+import { ethers, Contract, utils } from 'ethers';
 import { TokenMetadata, TokenSymbol } from '../src/types';
 import { CompoundService } from '../src/compound.service';
 import { AppController } from '../src/app.controller';
@@ -59,9 +59,8 @@ describe('Compound API (e2e)', () => {
             .get('/token-balance?token=WETH')
             .expect(200)
             .expect(res => {
-                if (!('token' in res.body)) throw new Error('missing token key');
-                if (!('amount' in res.body)) throw new Error('missing amount key');
-                const isMatch = /[0-9]*\.?[0-9]+/.test(res.body.amount);
+                if (!('WETH' in res.body)) throw new Error('missing token key');
+                const isMatch = /[0-9]*\.?[0-9]+/.test(res.body.WETH);
                 if (!isMatch) throw new Error(`Is not numeric value: ${res.body.amount}`);
             });
     });
@@ -69,19 +68,46 @@ describe('Compound API (e2e)', () => {
     it('/supply (POST)', async () => {
         const tokenService = moduleFixture.get<TokenService>(TokenService);
         const moneyMarketContract = moduleFixture.get<Contract>('money-market-contract');
+        const delta = '0.0001';
+        const req = request(app.getHttpServer());
 
         const lockTx = await tokenService.lockToken(TokenSymbol.WETH, moneyMarketContract.address);
-
         await lockTx.wait();
 
-        return request(app.getHttpServer())
-            .post('/supply?token=WETH&amount=0.0001&needAwaitMining=true')
-            .expect(201);
+        const rawTokenBalance = (await req.get('/token-balance?token=WETH')).body.WETH;
+        const tokenBalance = utils.parseEther(rawTokenBalance);
+        expect(tokenBalance.gte(utils.parseEther(delta))).toBeTruthy();
+
+        const rawSupplyBalance = (await req.get('/supply-balance?token=WETH')).body.WETH;
+        const supplyBalance = utils.parseEther(rawSupplyBalance);
+
+        await req.post(`/supply?token=WETH&amount=${delta}&needAwaitMining=true`);
+
+        const expectedTokenBalance = tokenBalance.sub(utils.parseEther(delta));
+        await req.get('/token-balance?token=WETH').then(x => {
+            expect(utils.parseEther(x.body.WETH).eq(expectedTokenBalance)).toBeTruthy();
+        });
+
+        const expectedSupplyBalance = supplyBalance.add(utils.parseEther(delta));
+        await req.get('/supply-balance?token=WETH').then(x => {
+            // gte, because between transaction and check some interest can be accumulated
+            expect(utils.parseEther(x.body.WETH).gte(expectedSupplyBalance)).toBeTruthy();
+        });
     });
 
     it('/withdraw (POST)', async () => {
-        return request(app.getHttpServer())
-            .post('/withdraw?token=WETH&amount=0.0001&needAwaitMining=true')
-            .expect(201);
+        const delta = '0.0001';
+        const req = request(app.getHttpServer());
+
+        const rawTokenBalance = (await req.get('/token-balance?token=WETH')).body.WETH;
+        const tokenBalance = utils.parseEther(rawTokenBalance);
+
+        await req.post(`/withdraw?token=WETH&amount=${delta}&needAwaitMining=true`);
+
+        const expectedTokenBalance = tokenBalance.add(utils.parseEther(delta));
+        await req.get('/token-balance?token=WETH').then(x => {
+            expect(utils.parseEther(x.body.WETH).eq(expectedTokenBalance)).toBeTruthy();
+        });
+
     });
 });
