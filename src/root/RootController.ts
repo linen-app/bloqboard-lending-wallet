d2af804c2f654cbde1d7f8a0e173c87cc0b209f5
Alex Bazhanau <bazhanau@outlook.com>
2018-12-28 15:37:25 -0800

    RootController removed from docs

--- src/root/RootController.ts
+++ src/root/RootController.ts
@@ -1,9 +1,11 @@
 import { Controller, Res, Get } from '@nestjs/common';
 import { Response } from 'express-serve-static-core';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller('/')
 export class RootController {
     @Get()
+    @ApiExcludeEndpoint()
     redirect(@Res() res: Response) {
         res.redirect('/api');
     }
